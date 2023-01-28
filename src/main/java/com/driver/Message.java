4e8fe2a7b6d56e418690a44462a2b47627f125e5
Faiz <syclops40116@gmail.com>
2023-01-28 19:31:51 +0530

    Third Attempt

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -17,6 +17,7 @@ public class Message {
         this.content = content;
         this.timestamp = new Date();
     }
+    
     public Message(int id, String content, Date timestamp) {
         this.id = id;
         this.content = content;
--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -28,7 +28,7 @@ public class WhatsappRepository {
     
     public String createUser(String name, String mobile) throws Exception {
     	if(userMobile.contains(mobile))
-    		throw new Exception("User already exists!");
+    		throw new Exception("User already exists");
     	
     	userMobile.add(mobile);
     	return "SUCCESS";
